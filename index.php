d10aa8c1fd8e0271f45211ea5871dcf06f1860f2
AJINKYA CHAVAN <87532290+AJINKYACHAVAN5@users.noreply.github.com>
2023-03-21 21:02:56 +0530

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php'); ?>
-<?php if(isset($_SESSION['uid'])): ?>
-	<?php include('main.php'); ?>
-<?php else: ?>
-	<?php include('login.php'); ?>
-<?php endif; ?>
-<?php include('footer.php'); ?>
